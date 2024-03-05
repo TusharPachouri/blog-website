e6a8e9e1cb1ea33472f592cb7c8d29c2f7c0504f
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-03-06 01:20:33 +0530

    Add .env file and update dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
+// import dotenv from "dotenv";
+// dotenv.config({
+//   path: "./.env",
+// });
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [data, setData] = useState(null);
 
   useEffect(() => {
@@ -19,35 +23,18 @@ function App() {
       });
   }, []);
   // console.log(data);
+  console.log(import.meta.env.VITE_REACT_APP_HOST);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        {data && (
-          <div>
-            <h2>Response from server:</h2>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-          </div>
-        )}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data && (
+        <div>
+          <h2>Response from server:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+      <h2>HOST: {import.meta.env.VITE_REACT_APP_HOST}</h2>
+      <h1>react app</h1>
     </>
   );
 }
