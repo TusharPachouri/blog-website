594f381869ac66d5445b6474c4e4c69cb09fd2ba
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-03-12 02:32:52 +0530

    Commented out Access-Control-Allow-Origin middleware

--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(
   cors({
