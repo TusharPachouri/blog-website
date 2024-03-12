f485c284e32ba558c1a222be1656627f5c54dd5d
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-03-12 21:55:57 +0530

    Update API proxy URL in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://blog-website-server-two.vercel.app",
+      // "/api": "http://localhost:8080",
+      "/api": "https://blog-website-server-two.vercel.app/",
     },
   },
   plugins: [react()],
