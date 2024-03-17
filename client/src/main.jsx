f88122b1ebb9e9560604ed9cbcb260f70f91b91c
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-03-17 15:05:05 +0530

    Update SpeedInsights import in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
