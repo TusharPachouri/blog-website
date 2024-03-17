d917564b54ad6563561e3b9de55c8dbcce666648
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-03-17 15:01:32 +0530

    Add @vercel/speed-insights package

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Analytics/>
+    <SpeedInsights/>
   </React.StrictMode>,
 )
